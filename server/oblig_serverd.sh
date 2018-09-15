73c0c98a1eb4db83120550e9e8f918256ea14e0b
Christian Scott <christian@kreativitet.no>
2018-09-15 10:17:45 +0200

    Adds service components

--- server/oblig_serverd.sh
+++ server/oblig_serverd.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-#/usr/local/bin/serverd.sh
+#/usr/local/bin/oblig_serverd.sh
 #
 
 function d_start()
 {
-    echo "Server: starting service"
+    echo "Oblig Server: starting service"
     server 80
     sleep 5
-    echo "PID id $(cat /tmp/server.pid)"
+    echo "PID id $(cat /tmp/oblig_server.pid)"
 }
 
 function d_stop()
 {
-    echo "Server: stopping service Service (PID = $(cat /tmp/server.pid))"
-    kill $(cat /tmp/server.pid)
-    rm /tmp/server.pid
+    echo "Oblig Server: stopping service Service (PID = $(cat /tmp/server.pid))"
+    kill $(cat /tmp/oblig_server.pid)
+    rm /tmp/oblig_server.pid
 }
 
 function d_status()
 {
-    ps -ef | grep server | grep -v grep
-    echo "PID indicate indication file $(cat /tmp/server.pid 2> /dev/null)"
+    ps -ef | grep oblig_server | grep -v grep
+    echo "PID indicate indication file $(cat /tmp/oblig_server.pid 2> /dev/null)"
 }
 
 case "$1" in
